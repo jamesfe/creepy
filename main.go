a4ea1b90a24f5944725bc1be7e86f7046f5c6fdd
jamesfe <james.ferrara@gmail.com>
2017-03-09 11:12:41 +0100

    Initial

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import "github.com/google/gopacket"
+import "github.com/google/gopacket/pcap"
+import "github.com/google/gopacket/layers"
+
+import "errors"
+import "fmt"
+import "flag"
+
+var (
+	pcapFile string
+	handle   *pcap.Handle
+	err      error
+)
+
+type Dot11Tag struct {
+	Type    int
+	Length  int
+	Payload []byte
+}
+
+type Dot11ProbeRequest struct {
+	Tags []Dot11Tag
+}
+
+func getTagAndLoad(inData []byte) (Dot11Tag, int, error) {
+	if len(inData) >= 2 {
+		tag := int(inData[0])
+		length := int(inData[1])
+		if len(inData) > length {
+			payload := inData[2 : 2+length]
+			return Dot11Tag{Type: tag, Length: length, Payload: payload}, length + 2, nil
+		} else {
+			return Dot11Tag{}, -1, errors.New("array too short, < 2")
+		}
+	} else {
+		return Dot11Tag{}, -1, errors.New("array too short < len")
+	}
+}
+
+func IsDot11ProbeRequestPacket(inPacket, gopacket.Packet) bool {
+	wlan := packet.Layer(layers.LayerTypeDot11)
+	if wlan != nil {
+		wlanPacket, _ := wlan.(*layers.Dot11)
+		if wlanPacket.Type == 0x10 {
+			return true
+		}
+	}
+	return false
+}
+
+func GetDot11ProbeRequest(inPacket gopacket.Packet) (Dot11ProbeRequest, error) {
+	return Dot11ProbeRequest{}, nil
+}
+
+func main() {
+	var filename = flag.String("filename", "", "input filename")
+	flag.Parse()
+	pcapFile = *filename
+
+	// Open file instead of device
+	handle, err = pcap.OpenOffline(pcapFile)
+	if err != nil {
+		panic(fmt.Sprintf("Bad! %s", err))
+	}
+	defer handle.Close()
+
+	// Loop through packets in file
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+		wlan := packet.Layer(layers.LayerTypeDot11)
+		if wlan != nil {
+			wlanPacket, _ := wlan.(*layers.Dot11)
+			if wlanPacket.Type == 0x10 {
+				var decomposedPacket []Dot11Tag
+				fmt.Println(wlanPacket.Type)
+				payload := wlanPacket.LayerPayload()
+				fmt.Printf("Payload: \n %#X\n", payload)
+				start := 0
+				fmt.Printf("Length of payload: %d\n", len(payload))
+				for start < len(payload) {
+					pl, rem, err := getTagAndLoad(payload[start:])
+					if err == nil {
+						decomposedPacket = append(decomposedPacket, pl)
+						start += rem
+					} else {
+						fmt.Println("Something went wrong. %s\n", err)
+						break // we should quit while we are ahead
+					}
+				}
+			}
+		}
+	}
+}
